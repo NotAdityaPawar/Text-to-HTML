3f5b58744734b30e81829b26e113705dcc1a8ecf
Aditya Pawar <adityapawar2019@gmail.com>
2021-12-24 16:18:57 +0530

    Added the mvp and base.html

--- text2html/mainapp/urls.py
+++ text2html/mainapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 
 
 from django.urls import path,include
+from .views import NormalTextView
+
 
 urlpatterns = [
-
+    path('',NormalTextView.as_view()),
 ]
--- text2html/mainapp/views.py
+++ text2html/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+from .models import NoramlText
+class NormalTextView(generic.ListView):
+    model = NoramlText
+    template_name = 'mainfile.html'
