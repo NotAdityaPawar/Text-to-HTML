e09bbf8382e9a34eec093a50ca0fb11d12c93620
Aditya Pawar <adityapawar2019@gmail.com>
2021-12-24 15:21:32 +0530

    Added the model

--- text2html/mainapp/models.py
+++ text2html/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class NoramlText(models.Model):
+    content = models.TextField()
+
+
+    def __str__(self):
+        return self.content
